1d135e0867fa15f1cca660936960a043cd606e18
CrZto7 <christogeorge09@gmail.com>
2026-01-03 14:11:02 +0530

    Search,Community & Itinerary View
    Built using HTML and CSS

--- app.py
+++ app.py
@@ -149,5 +149,17 @@ def build_itinerary():
 def trip_listing():
     return render_template('trip_listing.html')
 
+@app.route('/search')
+def activity_search():
+    return render_template('activity_search.html')
+
+@app.route('/itineraryview')
+def itinerary_view():
+    return render_template('itinerary_view.html')
+
+@app.route('/communitytab')
+def community_tab():
+    return render_template('community_tab.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
